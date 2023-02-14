b4f5edafce1e51c51e8dd8b739ccc29e63a544e6
Akino02 <romster2012@seznam.cz>
2023-02-14 18:51:05 +0100

    first commit

---
+++ res/js/main.js
@@ -0,0 +1,94 @@
+const start = document.getElementById("startbutt");
+const game = document.getElementById("container_game");
+const taker = document.getElementById("taker");
+const scorehit = document.getElementById("scorehit");
+const music = document.getElementById("music");
+const musicvol = document.getElementById("musicvol");
+const musicgood = document.getElementById("musicgood");
+const hpball = document.getElementById("hp");
+const hpbarview = document.getElementById("hpbar");
+
+//const test = document.getElementsByClassName("test");
+
+
+//ZKUSIT TO UDĚLAT NA JAKOUKOLIV HUDBU CO JSI ČLOVĚK ZVOLÍ !!!!!
+
+
+let x = 0;
+let y = 0;
+let z = 7;
+let countclick = 0;
+let hpbar = 390;
+
+//randomnumber pro kruh pozice
+function randomnumber() {
+  setInterval(() => {
+    x = Math.floor(Math.random() * 1400 + 100);
+    y = Math.floor(Math.random() * 700 + 100);
+  }, 1000);
+}
+//když se dohraje hra tak se vypne hudba a jde to do menu
+function musicplay() {
+  setInterval(() => {
+    if (music.currentTime >= 218 || hpbar == 0) {
+      music.pause();
+      start.style.display = "block";
+      musicvol.style.display = "block";
+      musicgood.style.display = "block";
+      game.style.display = "none";
+    }
+  }, 1);
+}
+
+//animace score
+function scoreanime() {
+  scorehit.style.transition = "0.1s";
+}
+//funkce pro životy když neklikne na nic tak je konec hry
+function hpfunction() {
+  setInterval(() => {
+    if (hpbar > 0) {
+      hpbar--;
+      hpball.style.left = `${hpbar}px`;
+    }
+    else if(hpbar == 0){
+      music.pause();
+    }
+  }, 10);
+}
+musicgood.onclick = () => {
+  z = musicvol.value;
+  if (z == 10) {
+    music.volume = z - 9;
+    alert("Music Volume was changed");
+  } else if (musicvol.value == "") {
+    alert("Nothing was changed");
+  } else {
+    music.volume = `0.${z}`;
+    alert("Music Volume was changed");
+  }
+  musicvol.value = "";
+};
+start.onclick = () => {
+  x = Math.floor(Math.random() * 1400 + 100);
+  y = Math.floor(Math.random() * 700 + 100);
+  start.style.display = "none";
+  game.style.display = "block";
+  hpbarview.style.display = "block";
+  music.play();
+  musicplay();
+  hpfunction();
+  musicvol.style.display = "none";
+  musicgood.style.display = "none";
+};
+taker.onclick = () => {
+  scoreanime();
+  scorehit.style.display = "block";
+  scorehit.style.top = taker.style.top;
+  scorehit.style.left = taker.style.left;
+  countclick++;
+  randomnumber();
+  taker.style.top = `${y}px`;
+  taker.style.left = `${x}px`;
+  hpbar += 60;
+};
