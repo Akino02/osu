cc0066f7d54da5af006e32f142f102f81a00087c
Akino02 <romster2012@seznam.cz>
2023-02-26 09:51:22 +0100

    Update_1.2

--- res/js/main.js
+++ res/js/main.js
@@ -1,4 +1,5 @@
 const start = document.getElementById("startbutt");
+const menu = document.getElementById("menu");
 const game = document.getElementById("container_game");
 const taker = document.getElementById("taker");
 const scorehit = document.getElementById("scorehit");
@@ -7,60 +8,84 @@ const musicvol = document.getElementById("musicvol");
 const musicgood = document.getElementById("musicgood");
 const hpball = document.getElementById("hp");
 const hpbarview = document.getElementById("hpbar");
-
-//const test = document.getElementsByClassName("test");
-
+const resulttab = document.getElementById("result");
+const shutres = document.getElementById("shutres");
+const score = document.getElementById("score");
+const greathit = document.getElementById("greathit");
+const misclick = document.getElementById("misclick");
 
 //ZKUSIT TO UDĚLAT NA JAKOUKOLIV HUDBU CO JSI ČLOVĚK ZVOLÍ !!!!!
-
+//Udělat na jiné obtížnosti
+//udělat timer hudby    /priorita !!!! (harore)
+//udělat aby mohl missnout click     / v podstate je ale moc nejde
+//na konci udělat tabulku kolikrát kliknul a kolikrát missnul
 
 let x = 0;
 let y = 0;
 let z = 7;
 let endspam = 0;
 let countclick = 0;
+let misclicks = 0;
 let hpbar = 390;
+let scorenum = 0;
+
+//kontrola výšky a šířky zda to sedí zda ne tak tam bude upozornění, že to nefituje
+/*window.addEventListener("resize", function() {
+  if (window.matchMedia("(min-width: 1400px)").matches && window.matchMedia("(min-width: 100px)").matches) {
+    console.log("Screen width is 1400px to 100px")
+  } else {
+    console.log("Screen doesn't fit with width")
+  }
+})
+window.addEventListener("resize", function() {
+  if (window.matchMedia("(min-height: 700px)").matches && window.matchMedia("(min-height: 100px)").matches) {
+    console.log("Screen height is 700px to 100px")
+  } else {
+    console.log("Screen doesn't fit with height")
+  }
+})*/
 
 //randomnumber pro kruh pozice
 function randomnumber() {
   setInterval(() => {
     x = Math.floor(Math.random() * 1400 + 100);
     y = Math.floor(Math.random() * 700 + 100);
-  }, 1000);
-}
-//když se dohraje hra tak se vypne hudba a jde to do menu
-function musicplay() {
-  setInterval(() => {
-    if (music.currentTime >= 218 || hpbar == 0) {
-      music.pause();
-      start.style.display = "block";
-      musicvol.style.display = "block";
-      musicgood.style.display = "block";
-      game.style.display = "none";
-      hpbarview.style.display = "none";
-    }
   }, 1);
 }
-
-//animace score
-function scoreanime() {
-  scorehit.style.transition = "0.1s";
-}
+//funkce když se dohraje hra tak se vypne hudba a jde to do menu
 //funkce pro životy když neklikne na nic tak je konec hry
-function hpfunction() {
-  if (endspam == 0){
+function musicplay_hpfunction() {
+  setInterval(() => {
+    if (music.currentTime >= 218 || hpbar <= 20) {
+      music.pause();
+      menu.style.display = "flex";
+      game.style.display = "none";
+      resulttab.style.display = "block";
+    }
+  }, 10);
+  if (endspam == 0) {
     setInterval(() => {
-      if (hpbar > 0) {
+      if (hpbar > 20 && game.style.display == "block") {
         hpbar--;
         endspam++;
         hpball.style.left = `${hpbar}px`;
-      }
-      else if(hpbar == 0){
+      } else if (hpbar <= 20) {
         music.pause();
       }
-    }, 10);
+    }, 20);
   }
 }
+function resultwrite() {
+    score.innerHTML = `Score: ${scorenum}`;
+    greathit.innerHTML = `Hits: ${countclick}`;
+    misclick.innerHTML = `Misclicks: ${misclicks - countclick}`;
+}
+window.onload = () => {
+  randomnumber();
+  taker.style.top = `${y}px`;
+  taker.style.left = `${x}px`;
+};
+
 musicgood.onclick = () => {
   z = musicvol.value;
   if (z == 10) {
@@ -75,33 +100,51 @@ musicgood.onclick = () => {
   musicvol.value = "";
 };
 start.onclick = () => {
+  randomnumber();
   hpbar = 390;
+  music.currentTime = 0;
   hpball.style.left = `${hpbar}px`;
   hpbarview.style.display = "block";
-  hpfunction();
   music.play();
-  musicplay();
-  x = Math.floor(Math.random() * 1400 + 100);
-  y = Math.floor(Math.random() * 700 + 100);
-  start.style.display = "none";
+  musicplay_hpfunction();
+  taker.style.top = `${y}px`;
+  taker.style.left = `${x}px`;
+  menu.style.display = "none";
   game.style.display = "block";
-  musicvol.style.display = "none";
-  musicgood.style.display = "none";
-  music.currentTime = 0;
+  scorehit.style.display = "none";
+  scorenum = 0;
+  misclicks = 0;
+  countclick = 0;
+  resultwrite();
+};
+game.onclick = () => {
+  randomnumber();
+  taker.style.top = `${y}px`;
+  taker.style.left = `${x}px`;
+  misclicks++;
+  resultwrite();
 };
 taker.onclick = () => {
-  scoreanime();
   scorehit.style.display = "block";
   scorehit.style.top = taker.style.top;
   scorehit.style.left = taker.style.left;
   countclick++;
+  if (hpbar < 340) {
+    hpbar += 60;
+  } else if (hpbar >= 340) {
+    hpbar = 390;
+  } else {
+    hpbar += 0;
+  }
   randomnumber();
   taker.style.top = `${y}px`;
   taker.style.left = `${x}px`;
-  if(hpbar < 340){
-    hpbar += 60;
-  }
-  else{
-    hpbar += 0;
-  }
+  scorenum += (countclick*100);
+  resultwrite();
+};
+shutres.onclick = () => {
+  resulttab.style.display = "none";
+  hpbar = 390;
+  music.currentTime = 0;
+  musicplay_hpfunction();
 };
